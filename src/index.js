4ce4b54837585f8f67ac6e64db777260487ef105
Shelby Cook <shelby.elle.cook@gmail.com>
2020-07-29 16:54:34 -0500

    setting up index.js and garden.js component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import axios from 'axios';
+import logger from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+import { takeEvery, put } from 'redux-saga/effects';
 
 import App from './App';
 
@@ -9,20 +13,78 @@ import App from './App';
 const startingPlantArray = [
   { id: 1, name: 'Rose' },
   { id: 2, name: 'Tulip' },
-  { id: 3, name: 'Oak' }
+  { id: 3, name: 'Oak' },
 ];
 
 const plantList = (state = startingPlantArray, action) => {
   switch (action.type) {
     case 'ADD_PLANT':
-      return [ ...state, action.payload ]
+      return [...state, action.payload];
     default:
       return state;
   }
 };
 
+function* rootSaga(action) {
+  yield takeEvery('GET_PLANT', getPlant);
+  yield takeEvery('POST_PLANT', postPlant);
+  // register all of our sagas
+}
+
+//CREATE SAGAS - GET
+function* getPlant(action) {
+  try {
+    const response = yield axios.get('/plant');
+    yield put({
+      type: 'SET_GARDEN', //DISPATCHING RESPONSE TO STATE (REDUCER)
+      payload: response.data,
+    });
+    console.log(response);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+function* postPlant(action) {
+  try {
+    yield axios.post('/plant', action.payload);
+    // put = this.props.dispatch()
+    yield put({
+      type: 'GET_PLANT', //CALLING THE GET_FRUIT SAGA TO RUN, POST --> GET
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// Create sagaMiddleware
+const sagaMiddleware = createSagaMiddleware();
+
+const plantReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'SET_PLANT':
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+const sagaMiddleware = createSagaMiddleware();
+
 const store = createStore(
-  combineReducers({ plantList }),
+  combineReducers({
+    plantList,
+    getPlant,
+    postPlant,
+  }),
+  applyMiddleware(sagaMiddleware, logger)
 );
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('react-root'));
+sagaMiddleware.run(rootSaga);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('react-root')
+);
