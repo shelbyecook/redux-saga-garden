61455a1e5d0d8d333f5ce7cd8e1f05cc8074fddd
Shelby Cook <shelby.elle.cook@gmail.com>
2020-07-30 13:46:45 -0500

    creating plant list

--- src/components/PlantList/PlantList.js
+++ src/components/PlantList/PlantList.js
@@ -1,23 +1,57 @@
 import React, { Component } from 'react';
+import './PlantList.css';
 import { connect } from 'react-redux';
 
-const mapStateToProps = reduxState => ({
-    reduxState,
-});
-
 class PlantList extends Component {
-    componentDidMount() {
-        // use component did mount to dispatch an action to request the plantList from the API
-    }
+  componentDidMount() {
+    // use component did mount to dispatch an action to request the plantList from the API
+    this.props.dispatch({
+      type: 'GET_PLANTS',
+    });
+  }
 
-    render() {
-        return (
-            <div>
-                <h3>This is the plant list</h3>
-                <pre>{JSON.stringify(this.props.reduxState)}</pre>
-            </div>
-        );
-    }
+  render() {
+    console.log(this.props.plantList);
+
+    return (
+      <div>
+        <h3>This is the plant list</h3>
+        <pre>{JSON.stringify(this.props.reduxState)}</pre>
+        <table className="list">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Kingdom</th>
+              <th>Clade</th>
+              <th>Order</th>
+              <th>Family</th>
+              <th>Sub-Family</th>
+              <th>Genus</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.plantList.map((item, index) => {
+              return (
+                <tr key={item.id}>
+                  <td>{item.name}</td>
+                  <td>{item.kingdom}</td>
+                  <td>{item.clade}</td>
+                  <td>{item.order}</td>
+                  <td>{item.family}</td>
+                  <td>{item.subfamily}</td>
+                  <td>{item.genus}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
+
+const mapStateToProps = (store) => ({
+  plantList: store.plantList,
+});
 
 export default connect(mapStateToProps)(PlantList);
