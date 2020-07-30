ed5f3acebad352681335590194e8fb43b4837fd1
Shelby Cook <shelby.elle.cook@gmail.com>
2020-07-30 13:50:06 -0500

    cleaning up console logs and components

--- src/components/Garden/Garden.js
+++ src/components/Garden/Garden.js
@@ -1,14 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import NewPlantForm from '../NewPlantForm/NewPlantForm';
 import PlantList from '../PlantList/PlantList';
-// import axios from 'axios';
-import { connect } from 'react-redux';
 
 const Garden = (props) => (
   <div>
     <h2>This is the garden!</h2>
-    {/* Redux State isn't needed in the garden, it is just a parent component */}
-    {/* Thanks to redux, there is no need to pass along props! */}
     <NewPlantForm />
     <PlantList />
   </div>
