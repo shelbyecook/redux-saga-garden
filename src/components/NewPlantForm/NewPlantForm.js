7900932813e184eb5a2151321660275f74ce24e4
Shelby Cook <shelby.elle.cook@gmail.com>
2020-07-30 14:29:01 -0500

    adding input labels

--- src/components/NewPlantForm/NewPlantForm.js
+++ src/components/NewPlantForm/NewPlantForm.js
@@ -52,49 +52,56 @@ class NewPlantForm extends Component {
         <h3>This is the form</h3>
         {/* <pre>{JSON.stringify(this.state)}</pre> */}
         <form onSubmit={this.addNewPlant}>
+          <label for="name">Name</label>
           <input
             type="text"
             name="name"
             value={this.state.newPlant.name}
             onChange={this.handleNameChange('name')}
           />
+          <label for="kingdom">Kingdom</label>
           <input
             type="text"
             name="kingdom"
             value={this.state.newPlant.kingdom}
             onChange={this.handleNameChange('kingdom')}
           />
+          <label for="clade">Clade</label>
           <input
             type="text"
             name="clade"
             value={this.state.newPlant.clade}
             onChange={this.handleNameChange('clade')}
           />
+          <label for="order">Order</label>
           <input
             type="text"
             name="order"
             value={this.state.newPlant.order}
             onChange={this.handleNameChange('order')}
           />
+          <label for="family">Family</label>
           <input
             type="text"
             name="family"
             value={this.state.newPlant.family}
             onChange={this.handleNameChange('family')}
           />
+          <label for="subfamily">Subfamily</label>
           <input
             type="text"
             name="subfamily"
             value={this.state.newPlant.subfamily}
             onChange={this.handleNameChange('subfamily')}
           />
+          <label for="genus">Genus</label>
           <input
             type="text"
             name="genus"
             value={this.state.newPlant.genus}
             onChange={this.handleNameChange('genus')}
           />
-
+          <label for="name">Name</label>
           <input type="submit" value="Add New Plant" />
         </form>
       </div>
